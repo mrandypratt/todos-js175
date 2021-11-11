a9172ccd091e4a38c3e70acb5d0d84a228e1b4fe
mrandypratt <mrandypratt@gmail.com>
2021-11-10 21:24:18 -0600

    refactored sort functions to module

--- todos.js
+++ todos.js
@@ -4,6 +4,7 @@ const flash = require("express-flash");
 const session = require("express-session");
 const { body, validationResult } = require("express-validator");
 const TodoList = require("./lib/todolist");
+const { sortTodoLists, sortTodos } = require("./lib/sort");
 
 const app = express();
 const host = "localhost";
@@ -33,35 +34,6 @@ app.use((req, res, next) => {
 
 const loadTodoList = todoListId => {
   return todoLists.find(todoList => todoList.id === todoListId);
-};
-
-const compareByTitle = (itemA, itemB) => {
-  let titleA = itemA.title.toLowerCase();
-  let titleB = itemB.title.toLowerCase();
-
-  if (titleA < titleB) {
-    return -1;
-  } else if (titleA > titleB) {
-    return 1;
-  } else {
-    return 0;
-  }
-};
-
-const sortTodoLists = lists => {
-  let undone = lists.filter(todoList => !todoList.isDone());
-  let done   = lists.filter(todoList => todoList.isDone());
-  undone.sort(compareByTitle);
-  done.sort(compareByTitle);
-  return [].concat(undone, done);
-};
-
-const sortTodos = todoList => {
-  let undone = todoList.todos.filter(todo => !todo.isDone());
-  let done   = todoList.todos.filter(todo => todo.isDone());
-  undone.sort(compareByTitle);
-  done.sort(compareByTitle);
-  return [].concat(undone, done);
 };
 
 app.get("/", (req, res) => {
